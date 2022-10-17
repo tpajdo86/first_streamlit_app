cc63b859cf96936da9cd5c99dd14ff984327745c
tpajdo86 <115372858+tpajdo86@users.noreply.github.com>
2022-10-17 22:59:34 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,11 +10,7 @@ streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞Avocado Toast')
 
-
-
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
-
-
 
 # Display the table on the page.
 
@@ -23,27 +19,27 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 
 # Let's put a pick list here so they can pick the fruit they want to include 
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
-
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 streamlit.dataframe(fruits_to_show)
 
 
 streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
-
-
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+  if not fruity_choice:
+     streamlit.write('The user entered ', fruit_choice)
+  else:
+      fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 # write your own comment -what does the next line do? 
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+      fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
-streamlit.dataframe(fruityvice_normalized)
+      streamlit.dataframe(fruityvice_normalized)
 
-
+except URLError as e:
+  streamlit.error()
+  
 streamlit.stop()
-
 
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
